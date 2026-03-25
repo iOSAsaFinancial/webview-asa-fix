09231a047a2ef27c56e04c91d04c23a8abcf46f0
Mykola <mykola.hrybeniuk@asa.financial>
2026-03-25 13:13:21 +0200

    fix

--- android/src/main/java/com/reactnativecommunity/webview/AsaWebViewClient.java
+++ android/src/main/java/com/reactnativecommunity/webview/AsaWebViewClient.java
@@ -13,7 +13,10 @@ public class AsaWebViewClient extends RNCWebViewClient {
         if (url != null) {
             String appScheme = view.getContext().getPackageName() + "://";
             if (url.startsWith(appScheme)) {
-                return true; // Block URLs using app package name as scheme
+                // Let the parent dispatch the event to JS (onShouldStartLoadWithRequest)
+                // but always block the URL to prevent ERR_UNKNOWN_URL_SCHEME.
+                super.shouldOverrideUrlLoading(view, url);
+                return true;
             }
         }
         return super.shouldOverrideUrlLoading(view, url);
